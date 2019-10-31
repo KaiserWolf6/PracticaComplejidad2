9a1d11eaaa94a82d85c3698a06b07c1cbdfdae54
Mauricio Suarez <kaiserwolf18@gmail.com>
2019-10-31 09:18:49 -0600

    Subsetsum

---
+++ SubsetSum.py
@@ -0,0 +1,12 @@
+import random 
+
+def subset-sum():
+	pass
+
+#Generamos 15 numeros al azar
+print("Genero 15 numeros al azar: ")
+desde = 1
+hasta = 20
+cantidad = 15
+#Genera los 15 numeros aleatorios
+lista = [random.uniform(desde,hasta) for x in range(cantidad)]
